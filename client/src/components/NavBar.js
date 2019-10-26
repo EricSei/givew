ffafd9b82b446e28ff58268ececf80c0bee52fa3
OrenBen-Meir <orenbm11@gmail.com>
2019-10-26 16:15:08 -0400

    improved navbar content font-size

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -22,19 +22,19 @@ class Navbar extends Component {
                             <div className="">
                                 <ul className="right hide-on-med-and-down">
                                     <li className="">
-                                        <Link to="/profile" className="">Profile</Link>
+                                        <Link to="/profile" className="quarter-bigger">Profile</Link>
                                     </li>
                                     <li className="">
-                                        <Link to="/signout" className="">Sign Out</Link>
+                                        <Link to="/signout" className="quarter-bigger">Sign Out</Link>
                                     </li>
                                     <li>
-                                        <Link to="/donate/create" className="">Create Donation</Link>
+                                        <Link to="/donate/create" className="quarter-bigger">Create Donation</Link>
                                     </li>
                                     <li>
-                                        <Link to="/items/reserved" className="">Reserved Items</Link>
+                                        <Link to="/items/reserved" className="quarter-bigger">Reserved Items</Link>
                                     </li>
                                     <li>
-                                        <Link to="/items/donated" className="">Donated Items</Link>
+                                        <Link to="/items/donated" className="quarter-bigger">Donated Items</Link>
                                     </li>
                                 </ul>
                             </div> 
