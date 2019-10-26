75b74011b56ebcadef5bcb33dac3eae564ea5fb5
OrenBen-Meir <orenbm11@gmail.com>
2019-10-26 15:55:48 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React                     from 'react'
 import { Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import Navbar  from './components/NavBar';
 import Landing from './components/LandingPage';
 import Signin  from './components/SigninPage';
 import history from './history';
@@ -9,6 +10,7 @@ import history from './history';
 const App = () => {
   return (
     <Router history={history}>
+      <Navbar />
       <div className="container">
         <Switch>
           <Route path="/signin" component={Signin} />
