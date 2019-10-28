950bfa505f6a85a421cd63e26359c87071c043cb
ericsei <eric.ahhpusei@gmail.com>
2019-10-28 13:10:09 -0400

    add signuppage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import React                     from 'react'
+import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import history            from './history';
-import { AuthProvider }   from './contexts/AuthContext';
-import useAuth            from './hooks/useAuth';
-import Navbar             from './components/NavBar';
-import Signout            from './components/SignOut';
-import Landing            from './components/LandingPage';
-import Signin             from './components/SigninPage';
+import history from './history';
+import { AuthProvider } from './contexts/AuthContext';
+import useAuth from './hooks/useAuth';
+import Navbar from './components/NavBar';
+import Signout from './components/SignOut';
+import Landing from './components/LandingPage';
+import Signin from './components/SigninPage';
+import Signup from './components/SignUpPage';
 import CreateDonationPage from './components/CreateDonationPage';
 
 const App = () => {
@@ -17,11 +18,12 @@ const App = () => {
   return (
     <Router history={history}>
       <div className="container">
-        <Navbar/>
+        <Navbar />
         <Switch>
           <Route path="/donate/create/" component={CreateDonationPage} />
           <Route path='/signout' component={Signout} />
           <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
           <Route path="/" component={Landing} />
         </Switch>
       </div>
