84eaf7554455a4fdac80a171b73d92fff2437df7
ericsei <eric.ahhpusei@gmail.com>
2019-11-05 22:54:54 -0500

    update Item Model

--- api/models/Item.js
+++ api/models/Item.js
@@ -1,60 +1,33 @@
 'use strict';
 const { Model } = require('sequelize');
-//const User = require('./User');
 
 module.exports = (sequelize, DataTypes) => {
-    class Item extends Model { }
+  class Item extends Model { }
 
-    Item.init({
+  Item.init({
 
-        name: { type: DataTypes.STRING, allowNull: false },
-        description: {
-            type: DataTypes.TEXT,
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: {
+      type: DataTypes.TEXT,
 
-        },
-        photos: {
-            type: DataTypes.ARRAY(DataTypes.INTEGER),
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW
-        },
-        location: {
-            type: DataTypes.TEXT,
+    },
+    location: {
+      type: DataTypes.TEXT,
 
-        },
-        donator_id: {
-            type: DataTypes.INTEGER,
-            // references: {
-            //     model: User,
-            //     key: 'id',
-            // }
-        },
-        receiver_id: {
-            type: DataTypes.INTEGER,
-            // references: {
-            //     model: User,
-            //     key: 'id',
-            // }
-        },
-        // category_id: {
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //         model: 'Category',
-        //         key: 'id',
-        //     }
-        // }
+    },
+    zipcode: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+      sequelize,
+      modelName: 'item'
+    });
 
-    }, {
-            sequelize,
-            modelName: 'item'
-        });
+  Item.associate = models => {
+    models.Item.belongsTo(models.User, { foreignKey: 'donatorId' });
+    models.Item.belongsTo(models.User, { foreignKey: 'receiverId' });
+  };
 
-    Item.associate = models => {
-        // Define associations here.
-    };
-
-    return Item;
+  return Item;
 };
 
