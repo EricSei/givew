9aa13efea4d6efc892dab9c1c5ea5d30207df10a
ericsei <eric.ahhpusei@gmail.com>
2019-11-05 21:51:54 -0500

    add Item model: WIP

---
+++ api/models/Item.js
@@ -0,0 +1,61 @@
+'use strict';
+const { Model } = require('sequelize');
+//const User = require('./User');
+
+module.exports = (sequelize, DataTypes) => {
+    class Item extends Model { }
+
+    Item.init({
+
+        name: { type: DataTypes.STRING, allowNull: false },
+        description: {
+            type: DataTypes.TEXT,
+
+        },
+        photos: {
+            type: DataTypes.ARRAY(DataTypes.INTEGER),
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        location: {
+            type: DataTypes.TEXT,
+
+        },
+        donator_id: {
+            type: DataTypes.INTEGER,
+            // references: {
+            //     model: User,
+            //     key: 'id',
+            // }
+        },
+        receiver_id: {
+            type: DataTypes.INTEGER,
+            // references: {
+            //     model: User,
+            //     key: 'id',
+            // }
+        }
+
+
+    }, {
+            sequelize,
+            modelName: 'item'
+        });
+
+    Item.associate = models => {
+        // Define associations here.
+    };
+
+    return Item;
+};
+
+// category_id: {
+        //     type: DataTypes.INTEGER,
+        //     references: {
+        //         model: 'Category',
+        //         key: 'id',
+        //     }
+        // }
