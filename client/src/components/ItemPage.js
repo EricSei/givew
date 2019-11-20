4e733c0403cebd63b4125b9b948da97a48361c6c
yongchengzhu <yongchengzhu002@gmail.com>
2019-11-20 15:17:39 -0500

    Implemented req modal.

--- client/src/components/ItemPage.js
+++ client/src/components/ItemPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import useMaterialize from '../hooks/useMaterialize';
+
 const ItemPage = props => {
-  console.log(props.location);
+  useMaterialize();
+  
   return (
     <div>
+      <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Enter a message:</h4>
+      <textarea class="materialize-textarea" data-length="120" placeholder="Your message" />
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div>
       <div>Photos: </div>
       {
         props.location.state.photos.map(photo => {
@@ -12,7 +24,7 @@ const ItemPage = props => {
       }
       <div>{props.location.state.name}</div>
       <div>{props.location.state.description}</div>
-      <button className="btn">Request Item</button>
+      <button className="btn modal-trigger" data-target="modal1">Request Item</button>
     </div>
   );
 }
