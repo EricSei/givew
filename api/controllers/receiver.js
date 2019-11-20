2ffdc3fe6dba757b1b23edc9e72fc97973c61b22
yongchengzhu <yongchengzhu002@gmail.com>
2019-11-20 14:13:13 -0500

    Implemented waitlist get.

--- api/controllers/receiver.js
+++ api/controllers/receiver.js
@@ -23,4 +23,13 @@ router.post('/waitlist/create', passport.isAuthenticated(), (req, res) => {
     .catch(err => { res.status(400).json({ error: err }) });
 });
 
+// @route GET /api/receiver/waitlist/
+// @desc Retrieve all rows from waitlist that belong to this receiver.
+// @access Private
+router.get('/waitlist/', passport.isAuthenticated(), (req, res) => {
+  Waitlist.findAll({ where: { recieverId: req.user.id } })
+    .then(result => { res.json(result) })
+    .catch(error => { res.status(400).json({ error }) });
+});
+
 module.exports = router;
