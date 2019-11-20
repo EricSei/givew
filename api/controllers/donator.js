eed61c28269efa5e0df162329d763119413c46dd
yongchengzhu <yongchengzhu002@gmail.com>
2019-11-20 14:36:11 -0500

    Implemented bidlist get single item

--- api/controllers/donator.js
+++ api/controllers/donator.js
@@ -19,4 +19,13 @@ router.get('/bidlist/', passport.isAuthenticated(), (req, res) => {
     .catch(error => { res.status(400).json({ error }) });
 });
 
+// @route GET /api/donator/bidlist/item/:id
+// @desc Retrieve a single item from donator's bidlist.
+// @access Private
+router.get('/bidlist/item/:id', passport.isAuthenticated(), (req, res) => {
+  Item.findOne({ where: { donatorId: req.user.id, id: req.params.id } })
+    .then(result => { res.json(result) })
+    .catch(error => { res.status(400).json({ error }) });
+});
+
 module.exports = router;
