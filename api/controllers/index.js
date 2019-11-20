98719981bdfef863a00b2a025b6f009a82f6d9d9
yongchengzhu <yongchengzhu002@gmail.com>
2019-11-20 14:07:38 -0500

    Implemented watlist create api.

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -8,6 +8,7 @@ const appConfigController = require('./appConfig.js');
 const authController = require('./auth');
 const userController = require('./user');
 const itemController = require('./item');
+const receiverController = require('./receiver');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
@@ -16,6 +17,7 @@ router.use('/application-configuration', appConfigController);
 router.use('/auth', authController);
 router.use('/user', userController);
 router.use('/item', itemController);
+router.use('/receiver', receiverController);
 
 
 module.exports = router;
