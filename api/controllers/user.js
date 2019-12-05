541b3be87d6500f6f8b1c8c0164fa09953d25b1d
ericsei <eric.ahhpusei@gmail.com>
2019-12-05 17:12:55 -0500

    add username change endpoint

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,4 +7,18 @@ router.get('/', passport.isAuthenticated(), (req, res) => {
   res.json(req.user);
 });
 
+router.put('/username', passport.isAuthenticated(), (req, res) => {
+
+  User.update(
+      { username: req.body.username },
+      { returning: true,
+        where:{ id: req.user.id}
+      }
+    )
+    .then(([updatedRow,[updatedName]]) => {
+      res.json({updatedName});
+  })
+ 
+});
+
 module.exports = router;
