3f3bfb08c8f8231a36905f441d0f0d10da932682
yongchengzhu <yongchengzhu002@gmail.com>
2019-12-07 10:48:58 -0500

    Implemented route @GET /api/donator/bidlist/item/:id.

--- api/controllers/donator.js
+++ api/controllers/donator.js
@@ -27,11 +27,5 @@ router.get('/bidlist/item/:id', passport.isAuthenticated(), (req, res) => {
     .then(result => res.json(result))
     .catch(error => res.status(400).json({ error }));
 });
-// router.get('/bidlist/item/:id', passport.isAuthenticated(), (req, res) => {
-//   Item.findOne({ where: { donatorId: req.user.id, id: req.params.id } })
-//     .then(result => { res.json(result) })
-//     .catch(error => { res.status(400).json({ error }) });
-// });
-
 
 module.exports = router;
