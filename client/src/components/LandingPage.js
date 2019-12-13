8874eff02d09d859097053e376a94d55f2415b07
yongchengzhu <yongchengzhu002@gmail.com>
2019-12-13 01:57:50 -0500

    Styled homepage.

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,17 +7,32 @@ const Landing = () => {
   let [items] = useItems();
   
   return (
-    <div>
-      {
-        items.map(item => {
-          return (
-            <Link key={item.id} to={{ pathname: '/item', state: item }}>
-              <div>{item.name}</div>
-              <img src={item.photos[0]} style={{ width: "150px", height: "auto" }} />
-            </Link>
-          )
-        })
-      }
+    <div className="landing-container">
+      <div className="row" style={{ margin: 0 }}>
+        <div className="col s12 m12 category-bar">
+          <div className="category selected">Latest</div>
+          <div className="category">Furnitures</div>
+          <div className="category">Electronics</div>
+          <div className="category">Appliances</div>
+          <div className="category">Others</div>
+        </div>
+      </div>
+      <div className="row main">
+        {
+          items.map(item => {
+            return (
+              <div className="col s12 m4 center item-container">
+                <Link key={item.id} to={{ pathname: '/item', state: item }}>
+                  <img src={item.photos[0]} style={{ width: "150px", height: "auto" }} />
+                  <div className="item-name">{item.name}</div>
+                  <div className="item-zipcode">ZIP {item.zipcode}</div>
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
+      
     </div>
   );
 };
